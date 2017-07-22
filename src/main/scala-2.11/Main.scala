c876000260327e3b11748d8eb291e297fa312c9a
repo-mohan <mohanraj.inbox@gmail.com>
2017-07-22 19:41:47 +1000

    added more more functionality

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -11,40 +11,40 @@ import net.jcazevedo.moultingyaml._
 import net.jcazevedo.moultingyaml.DefaultYamlProtocol._
 import scala.io.Source
 
+case class Extract(eSource: String, eType: String, eStgTableName: String)
+
+case class Transform(tSQL: String, tStgTableName: String)
+
+case class Load(lTargetName: String, lStgTableName: String)
+
+case class etlConfig(AppName :String,
+                     Extracts: List[Extract],
+                     Transforms: List[Transform],
+                     Loads: List[Load])
 object Main {
-  case class Extract(eSource: String, eType: String)
-
-  case class Transform(tSQL: String, tStgTableName: String)
-
-  case class Load(lTargetName: String, lStgTableName: String)
-
-  case class etlConfig(AppName :String,
-                       Extracts: List[Extract],
-                       Transforms: List[Transform],
-                       Loads: List[Load])
-
   def main(args: Array[String]): Unit = {
     //reading config file
     // replace the hard coded value with args(0)
-    fetchEtlConfig("C:\\HADOOP\\sparketl\\etlConfig.yml")
+    val appConfig = fetchEtlConfig(args(0))
 
-  /*  val spark = SparkSession
+
+  val spark = SparkSession
       .builder()
-      .appName("sparketl")
+      .appName(appConfig.AppName)
       .master("local[2]")
-      .getOrCreate()*/
+      .getOrCreate()
 
- //   import spark.implicits._
- //   val df = spark.read.json("file:/C:/HADOOP/spark/examples/src/main/resources/people.json")
- //   df.write.parquet("file:/C:/HADOOP/spark/examples/src/main/resources/people.parquet")
- //   print(df.count())
- //   spark.stop()
+    import spark.implicits._
+
+    ExtractSources(spark, appConfig)
+
+    spark.stop()
   }
 
-  private def fetchEtlConfig(path: String): Unit = {
+  private def fetchEtlConfig(path: String):etlConfig  = {
 
     object MyYamlProtocol extends DefaultYamlProtocol {
-      implicit val ExtractFormat = yamlFormat2(Extract)
+      implicit val ExtractFormat = yamlFormat3(Extract)
       implicit val TransformFormat = yamlFormat2(Transform)
       implicit val LoadFormat = yamlFormat2(Load)
       implicit val etlConfigFormat = yamlFormat4(etlConfig)
@@ -55,12 +55,25 @@ object Main {
 
     val strYaml = Source.fromFile(path).mkString
     val astYaml = strYaml.parseYaml
-    println(astYaml.prettyPrint)
+    //println(astYaml.prettyPrint)
     val ccYaml  = astYaml.convertTo[etlConfig]
-    ccYaml.Extracts.foreach{println}
+    ccYaml
+    //println(ccYaml.getClass)
+    //ccYaml.Extracts.foreach{println}
+  }
+  private def ExtractSources(spark: SparkSession , appConfig: etlConfig): Unit = {
+    appConfig.Extracts.foreach{obj_Extract =>
+
+      obj_Extract.eType match {
+        case "JSON" =>
+          val df = spark.read.json(obj_Extract.eSource)
+          df.createOrReplaceGlobalTempView(obj_Extract.eStgTableName)
+          print(obj_Extract.eStgTableName + " counts: " + df.count())
+      }
+    }
   }
 
-  private def TransformSqls(spark: SparkSession): Unit = {
+  private def TransformSqls(spark: SparkSession, appConfig: etlConfig): Unit = {
     println("process the sqls")
   }
   private def WriteOutput(spark: SparkSession): Unit = {
